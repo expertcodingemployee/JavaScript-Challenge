224ce8686c2c59927404a5d91d41ca32ba5f3f5f
Michael Lindsay-Burke <103588759+expertcodingemployee@users.noreply.github.com>
2022-07-12 19:05:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(window.document.title);
+// let sentenceNode = document.getElementById('sometext');
+sentenceNode = sentenceNode.toString()
+
+let sentence = sentenceNode;
+
+let upperSentence = sentence.toUpperCase();
+
+console.log(typeof(upperSentence))
+
